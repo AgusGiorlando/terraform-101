dc7a87e8552af02a60b1fe7f906a9471367c34d1
AgusRAS <giorlandoagustin@rentasoft.com.ar>
2024-08-17 13:19:14 -0300

    Source de modulo desde repositorio

--- example-01/pre/sample/main.tf
+++ example-01/pre/sample/main.tf
@@ -1,5 +1,6 @@
 module "sample" {
-  source = "../../../example-01/modules/sample"
+  #source = "../../../example-01/modules/sample"
+  source = "github.com/AgusGiorlando/terraform-101.git//example-01/modules/sample"
 
   environtment                         = "pre"
   google_compute_instance_name         = "intance1"

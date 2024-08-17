9a52471c9a50e894d9efc597bf9011f0832dfddf
AgusRAS <giorlandoagustin@rentasoft.com.ar>
2024-08-17 11:53:16 -0300

    Etapa 2 - Instancia
    - Hacer que .tfstate se guarde en el bucket
    - Creacion de instancia
    - Outputs de datos de instancia

--- example-00/state.tf
+++ example-00/state.tf
@@ -1,8 +1,8 @@
 # Descomentar una vez el storage este creado para configurar el remote state.
-#terraform {
-#  backend "gcs" {
-#    #Sustituir el nombre del bucket
-#    bucket = "{ NOMBRE_BUCKET }"
-#    prefix = "terraform/state/example-00/"
-#  }
-#}
+terraform {
+  backend "gcs" {
+    #Sustituir el nombre del bucket
+    bucket = "tf-state-udemy-001"
+    prefix = "terraform/state/example-00/"
+  }
+}
